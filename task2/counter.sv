d1721f054d6f97be1f9132b181fbb85f90a095b4
MAadinP <m.aadinpatel@gmail.com>
2024-10-24 10:10:12 +0100

    Initial Commit

---
+++ task2/counter.sv
@@ -0,0 +1,15 @@
+module counter #(
+    parameter BIT_WIDTH = 8
+)( 
+    input logic [BIT_WIDTH-1:0]     incr,
+    input logic                     clk,
+    input logic                     rst,
+    input logic                     en,
+    output logic [BIT_WIDTH-1:0]    count
+);
+
+always_ff @(posedge clk) 
+    if(rst) count <= {BIT_WIDTH{1'b0}};
+    else if (en) count <= count + incr;
+    else count <=  count;
+endmodule
